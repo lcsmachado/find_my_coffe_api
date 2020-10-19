c838d9ac9f2427bde401ebe241bae48bf94a40e3
Lucas Carvalho <lucascarvalhouibai@gmail.com>
2020-10-19 15:26:54 -0300

    Using JBuilder to show the Coffee Store data

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :stores, only: [ :index, :show ], defaults: { format: 'json' }
+  namespace :api do
+    namespace :v1 do
+      resources :stores, only: [ :index, :show ], defaults: { format: 'json' }
+    end
+  end
 end
